220817ee805888b13d22dfd6644742414cd6dcf1
Halyna Moldovianu <Beorgeos@gmail.com>
2025-05-27 16:16:20 +0200

    images added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import imageEgg from "../images/create_a_photo_of_a_poached_egg_with_a_royal_face_and_cup_of_thee_in_his_hand_na7dwor1f6lwtzld66a5_3.png";
+function App() {
+  return (
+    <div>
+      <h1>Egg timer</h1>
+      <img src={imageEgg} alt="crazy egg" width="500px" />
+    </div>
+  );
+}
+
+export default App;
