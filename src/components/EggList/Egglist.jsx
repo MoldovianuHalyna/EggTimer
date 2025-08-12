7e35eb24a90bfe8e033c20b97a6e980310d16535
Halyna Moldovianu <Beorgeos@gmail.com>
2025-08-12 20:47:21 +0200

    homepage started

--- src/components/EggList/Egglist.jsx
+++ src/components/EggList/Egglist.jsx
@@ -1,7 +1,17 @@
 import EgglistItem from "../EgglistItem/EgglistItem";
+import s from "./EggList.module.css";
 
 const Egglist = () => {
-  return <div>Item</div>;
+  return (
+    <div className={s.egglistContainer}>
+      <EgglistItem image="/hard-boiled.jpg" label="Hard Boiled Egg" />
+      <EgglistItem image="/soft-boiled.jpg" label="Soft Boiled Egg" />
+      <EgglistItem image="/scrumbled.jpg" label="Scrambled Egg" />
+      <EgglistItem image="/poached.jpg" label="Poached Egg" />
+      <EgglistItem image="/omelette.jpg" label="Omelette" />
+      <EgglistItem image="/sunny-side-up.jpg" label="Sunny-side Up Egg" />
+    </div>
+  );
 };
 
 export default Egglist;
